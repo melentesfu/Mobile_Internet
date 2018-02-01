46872df76fc08d52d10b51a1757da00ae3ee23ed
melentesfu <t5tame01@students.oamk.fi>
2018-02-01 20:05:46 +0200

    all files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera , CameraOptions } from '@ionic-native/camera';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public photo :any;
+  public base64Image :string;
 
-  }
+
+constructor(public navCtrl: NavController,private camera: Camera) {
+ }
+
+ ngOnInit(){
+  this.photo =[];
+ }
+
+ takePicture(){
+  const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.photo.push(this.base64Image);
+     this.photo.reverse();
+    }, (err) => {
+     // Handle error
+    });
+ }
 
 }
